9659f27947e8779146369621e06b3d64a33f4849
Vasagirisravani <123653273+Vasagirisravani@users.noreply.github.com>
2023-11-20 10:33:35 +0530

    Add files via upload

---
+++ generalization ceasarcipher.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int gcd(int a,int b)
+{
+	if(b==0)
+	return a;
+	return gcd(b,a%b);
+}
+int main()
+{
+	int a;
+	printf("values of 'a' not allowed(because they are not relatively prime with 26):\n");
+	for( a=0;a<26;a++)
+	{
+		if(gcd(a,26)!=1)
+		{
+			printf("%d",a);
+		}
+	}
+	printf("\n");
+	
+	return 0;
+}
