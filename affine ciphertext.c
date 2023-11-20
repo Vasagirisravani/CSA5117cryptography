d9a6d3644978bcbfa1d82451a3fd25b4a50dcacc
Vasagirisravani <123653273+Vasagirisravani@users.noreply.github.com>
2023-11-20 10:46:36 +0530

    Add files via upload

---
+++ affine ciphertext.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+char decryptchar(int c,int a,int b)
+{
+	return((a*(c-b))%26+26)%26+'A';
+}
+int main()
+{
+	char ciphertext[1000];
+	int a;
+	int b;
+	printf("enter the ciphertext:");
+	scanf("%s",ciphertext);
+	
+	int mostfrequent=ciphertext[0];
+	int secondmostfrequent=ciphertext[1];
+	
+	printf("finding possible keys...\n");
+	for( a=1;a<26;a++)
+	{
+		for( b=0;b<26;b++)
+		{
+			if(decryptchar(mostfrequent,a,b)==mostfrequent&&
+			   decryptchar(secondmostfrequent,a,b)==secondmostfrequent)
+			   {
+			   	printf("possible key found:a=%d,b=%d\n",a,b);
+			   }
+		}
+	}
+	return 0;
+}
